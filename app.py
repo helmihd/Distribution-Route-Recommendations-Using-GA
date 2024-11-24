e08e0a7395e6c9d1e88d6dea44a536e0d4e7bba6
helmihd <helmihd02@gmail.com>
2024-11-24 09:40:17 +0700

    commit

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    # Ambil data dari form
+    locations = []
+    index = 0
+    while f"latitude-{index}" in request.form:
+        latitude = request.form.get(f"latitude-{index}")
+        longitude = request.form.get(f"longitude-{index}")
+        items = request.form.get(f"items-{index}")
+        locations.append({
+            "latitude": latitude,
+            "longitude": longitude,
+            "items": items
+        })
+        index += 1
+    
+    # Render ke template result.html dengan data yang diinputkan
+    return render_template('result.html', locations=locations)
 
 if __name__ == '__main__':
     app.run(debug=True)
